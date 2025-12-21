880aec68fcd06e143ac4ceb11bc6d11581b9a1f6
bala-amypo <bala@amypo.in>
2025-12-21 10:36:27 +0000

    User commit

--- src/main/java/com/example/demo/controller/StolenDeviceController.java
+++ src/main/java/com/example/demo/controller/StolenDeviceController.java
@@ -1,41 +1,41 @@
-package com.example.demo.controller;
+// package com.example.demo.controller;
 
-import com.example.demo.entity.StolenDeviceReport;
-import com.example.demo.service.StolenDeviceService;
-import org.springframework.web.bind.annotation.*;
+// import com.example.demo.entity.StolenDeviceReport;
+// import com.example.demo.service.StolenDeviceService;
+// import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+// import java.util.List;
 
-@RestController
-@RequestMapping("/api/stolen-devices")
-public class StolenDeviceController {
+// @RestController
+// @RequestMapping("/api/stolen-devices")
+// public class StolenDeviceController {
 
-    private final StolenDeviceService stolenService;
+//     private final StolenDeviceService stolenService;
 
-    public StolenDeviceController(StolenDeviceService stolenService) {
-        this.stolenService = stolenService;
-    }
+//     public StolenDeviceController(StolenDeviceService stolenService) {
+//         this.stolenService = stolenService;
+//     }
 
-    @PostMapping
-    public StolenDeviceReport reportStolen(
-            @RequestBody StolenDeviceReport report) {
-        return stolenService.reportStolen(report);
-    }
+//     @PostMapping
+//     public StolenDeviceReport reportStolen(
+//             @RequestBody StolenDeviceReport report) {
+//         return stolenService.reportStolen(report);
+//     }
 
-    @GetMapping
-    public List<StolenDeviceReport> getAllReports() {
-        return stolenService.getAllReports();
-    }
+//     @GetMapping
+//     public List<StolenDeviceReport> getAllReports() {
+//         return stolenService.getAllReports();
+//     }
 
-    @GetMapping("/{id}")
-    public StolenDeviceReport getReportById(@PathVariable Long id) {
-        return stolenService.getReportById(id)
-                .orElseThrow();
-    }
+//     @GetMapping("/{id}")
+//     public StolenDeviceReport getReportById(@PathVariable Long id) {
+//         return stolenService.getReportById(id)
+//                 .orElseThrow();
+//     }
 
-    @GetMapping("/serial/{serialNumber}")
-    public List<StolenDeviceReport> getReportsBySerial(
-            @PathVariable String serialNumber) {
-        return stolenService.getReportsBySerial(serialNumber);
-    }
-}
+//     @GetMapping("/serial/{serialNumber}")
+//     public List<StolenDeviceReport> getReportsBySerial(
+//             @PathVariable String serialNumber) {
+//         return stolenService.getReportsBySerial(serialNumber);
+//     }
+// }
