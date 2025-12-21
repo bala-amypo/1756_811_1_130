945906c7ecedf2895e71ffaaaf13d40b0a207828
bala-amypo <bala@amypo.in>
2025-12-21 11:24:18 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/FraudAlertServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.FraudAlertRecord;
+import com.example.demo.repository.FraudAlertRecordRepository;
+import com.example.demo.service.FraudAlertService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FraudAlertServiceImpl implements FraudAlertService {
+
+    private final FraudAlertRecordRepository repository;
+
+    public FraudAlertServiceImpl(FraudAlertRecordRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public FraudAlertRecord save(FraudAlertRecord alert) {
+        return repository.save(alert);
+    }
+
+    @Override
+    public List<FraudAlertRecord> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public FraudAlertRecord getById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<FraudAlertRecord> getBySerial(String serialNumber) {
+        return repository.findBySerialNumber(serialNumber);
+    }
+
+    @Override
+    public List<FraudAlertRecord> getByClaim(Long claimId) {
+        return repository.findByClaimId(claimId);
+    }
+}
