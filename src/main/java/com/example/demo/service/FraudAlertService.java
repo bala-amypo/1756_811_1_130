acc290248e5d571199b797d207ba07587cd350f1
bala-amypo <bala@amypo.in>
2025-12-21 11:23:53 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/FraudAlertService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import com.example.demo.model.FraudAlertRecord;
+
+import java.util.List;
+
+public interface FraudAlertService {
+
+    FraudAlertRecord save(FraudAlertRecord alert);
+
+    List<FraudAlertRecord> getAll();
+
+    FraudAlertRecord getById(Long id);
+
+    List<FraudAlertRecord> getBySerial(String serialNumber);
+
+    List<FraudAlertRecord> getByClaim(Long claimId);
+}
